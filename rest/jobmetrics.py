f39ef5edd96f7b9e335ebc10d0fecd7fe97aa166
Rémi Palancher <remi-externe.palancher@edf.fr>
2015-10-16 16:00:11 +0200

    Large rewrite of backend API
    2 majors improvements are brought by this commit:
    
    - The code is now object-oriented with real classes. This way, error
      management and code maintenance are going to be easier over time.
    - All influxdb requests are now merged into only one request with
      all metrics, for much more performances.

--- rest/jobmetrics.py
+++ rest/jobmetrics.py
@@ -24,155 +24,230 @@ from ClusterShell.NodeSet import NodeSet
 import requests
 import json
 
-CONF = '/etc/jobmetrics/jobmetrics.conf'
 app = Flask(__name__)
-conf = None
 
 # valid periods with their associated interval group time
 periods = { '1h': '10s',
             '6h': '30s',
             '24h': '120s' }
 
-def get_job_state(cluster, job):
-    """Request the Slurm REST API of the cluster to check if the job actually
-       exists. Return NOTFOUND if not.
-    """
+class Conf(object):
 
-    url = "{api}/job/{job}" \
-              .format(api=conf.get(cluster, 'slurm-api'),
-                      job=job)
-    resp = requests.get(url=url)
-    if resp.status_code == 404:
-        state = 'NOTFOUND'
-    else:
+    def __init__(self, fpath='/etc/jobmetrics/jobmetrics.conf'):
+        self.conf = ConfigParser.RawConfigParser()
+        self.conf.read(fpath)
+        self.influxdb_server = self.conf.get('influxdb', 'server')
+        self.influxdb_db = self.conf.get('influxdb', 'db')
+        self.clusters = self.conf.sections().remove('influxdb')
+
+    def slurm_api(self, cluster):
+        return self.conf.get(cluster, 'slurm-api')
+
+class MetricsDB(object):
+
+    def __init__(self, conf):
+
+        self.base_url = conf.influxdb_server
+        self.db = conf.influxdb_db
+        self.url = "{base}/query".format(base=self.base_url)
+
+    def get_metrics_results(self, cluster, job, metrics, period):
+        """Get the metrics of the job on the cluster for the period in parameters.
+
+           It sends an HTTP request to InfluxDB service to download the metric
+           values in JSON format and returns a list.
+        """
+
+        time_group = periods[period]
+
+        metrics_s = "\"" + "\", \"".join(metrics) + "\""
+        req = "select mean(value) from {metrics} " \
+              "where time > now() - {period} " \
+              "and cluster = '{cluster}' " \
+              "and job = 'job_{job}' " \
+              "group by time({time_group}), node fill(0)" \
+                .format(metrics=metrics_s,
+                        period=period,
+                        cluster=cluster,
+                        job=job,
+                        time_group=time_group)
+
+        payload = {'db': self.db, 'q': req, 'epoch': 'ms'}
+        resp = requests.get(url=self.url, params=payload)
+        if resp.status_code == 404:
+            raise LookupError("metrics not found for job {job} on cluster " \
+                              "{cluster}".format(job=job, cluster=cluster))
         data = json.loads(resp.text)
-        state = data['job_state']
 
-    return state
+        # data is a dict with 'results' key that is itself a list of dict with
+        # 'series' key that is as well a list of dict, one dict per node/node
+        # association. Each dict has it own list of values. We have to compute the
+        # sum the values for all nodes at every timestampsi, for each metric.
+        #
+        # Ex:
+        #
+        # { "results": [
+        #   { "series": [
+        #     { "name": "cpu-system",
+        #       "tags": {"node":"cn1"},
+        #       "columns": ["time","mean"],
+        #       "values": [
+        #         ["2015-10-16T11:37:20Z",0],
+        #         ...
+        #         ["2015-10-16T12:37:10Z",0],
+        #         ["2015-10-16T12:37:20Z",0]
+        #       ]
+        #     },
+        #     { "name": "cpu-system",
+        #       "tags": {"node":"cn2"},
+        #       "columns": ["time","mean"],
+        #       "values": [
+        #         ["2015-10-16T11:37:20Z",0],
+        #         ["2015-10-16T11:37:30Z",0],
+        #         ...
+        #         ["2015-10-16T12:37:10Z",0],
+        #        ["2015-10-16T12:37:20Z",0]
+        #       ]
+        #     },
+        #
+        #     ( ... then cpu-system for cn3 ...)
+        #
+        #     { "name": "cpu-user",
+        #       "tags": {"node":"cn1"},
+        #       "columns": ["time","mean"],
+        #       "values": [
+        #         ["2015-10-16T11:37:20Z",0],
+        #         ["2015-10-16T11:37:30Z",0],
+        #         ...
+        #         ["2015-10-16T12:37:10Z",0],
+        #         ["2015-10-16T12:37:20Z",0]
+        #       ]
+        #     },
+        #
+        #     ( ... then cpu-user for cn[2-3] ...)
+        #
+        #     { "name": "cpus",
+        #       "tags": {"node":"admin"},
+        #       "columns": ["time","mean"],
+        #       "values": [
+        #         ["2015-10-16T11:37:20Z",0],
+        #         ["2015-10-16T11:37:30Z",0],
+        #         ...
+        #         ["2015-10-16T12:37:10Z",6],
+        #         ["2015-10-16T12:37:20Z",0]
+        #       ]
+        #     },
+        #     { "name": "memory-pss",
+        #       "tags": {"node":"cn1"},
+        #       "columns": ["time","mean"],
+        #       "values": [
+        #         ["2015-10-16T11:37:20Z",0],
+        #         ["2015-10-16T11:37:30Z",0],
+        #         ...
+        #         ["2015-10-16T12:37:10Z",0],
+        #         ["2015-10-16T12:37:20Z",0]
+        #       ]
+        #     },
+        #
+        #     ( ... then memory-pss for cn[2-3] ...)
+        #
+        #   ]}
+        # ]}
+        series = data['results'][0]['series']
 
-def stack_cpu_idle(metrics):
-    """Compute the sum of cpu usages in metrics dict in parameters to stack
-       the idle cpu time and append in into the dict.
-    """
+        results = {}
+        nodeset = NodeSet()
 
-    for timestamp, values in metrics.iteritems():
-        values.append(values[0]*100 - values[1] - values[2])
+        for serie in series:
+            metric = serie['name']
+            node = serie['tags']['node'].encode('utf-8')
 
-def get_metric_results(cluster, job, metric, period, group=''):
-    """Get the metrics of the job on the cluster for the period in parameters.
+            if node not in nodeset:
+                nodeset.update(node)
 
-       It sends an HTTP request to InfluxDB service to download the metric
-       values in JSON format and returns a list.
+            for pair in serie['values']:
+                timestamp = str(pair[0])
+                value = pair[1]
+                if not results.has_key(timestamp):
+                    results[timestamp] = list()
+                    for xidx in range(len(metrics)):
+                        if xidx == metrics.index(metric):
+                            results[timestamp].append(value)
+                        else:
+                            results[timestamp].append(0)
+                else:
+                    results[timestamp][metrics.index(metric)] += value
 
-       The group parameter is concatenated in the request to add optional
-       additional group by clause. This is typically used for nodes in
-       get_job_node_metrics() function.
-    """
+        return (results, nodeset)
 
-    time_group = periods[period]
+class SlurmAPI(object):
 
-    url = "{influxdb}/query".format(influxdb=conf.get('influxdb', 'server'))
-    req = "select mean(value) from \"{metric}\" " \
-          "where time > now() - {period} " \
-          "and cluster = '{cluster}' " \
-          "and job = 'job_{job}' " \
-          "group by time({time_group}){group} fill(0)" \
-            .format(metric=metric,
-                    period=period,
-                    cluster=cluster,
-                    job=job,
-                    time_group=time_group,
-                    group=group)
-    db = conf.get('influxdb', 'db')
+    def __init__(self, conf, cluster):
 
-    payload = {'db': db, 'q': req, 'epoch': 'ms'}
-    resp = requests.get(url=url, params=payload)
-    if resp.status_code == 404:
-        raise LookupError("metrics not found for job {job} on cluster " \
-                          "{cluster}".format(job=job, cluster=cluster))
-    data = json.loads(resp.text)
-    results = data['results'][0]['series']
+        self.base_url = conf.slurm_api(cluster)
 
-    return results
+    def job_state(self, job):
+        """Request the Slurm REST API of the cluster to check if the job actually
+           exists. Return NOTFOUND if not.
+        """
 
-def get_job_node_metrics(metrics, ns, cluster, job, metric, period):
-    """Get the node related metric for the cluster and the job for the period
-       in parameter. It compute the sum of all metric node series to get only
-       one global serie for all nodes. It appends the result into the metrics
-       dict in parameter.
-    """
+        url = "{base}/job/{job}" \
+                  .format(base=self.base_url,
+                          job=job)
+        resp = requests.get(url=url)
+        if resp.status_code == 404:
+            state = 'NOTFOUND'
+        else:
+            data = json.loads(resp.text)
+            state = data['job_state']
 
-    results = get_metric_results(cluster, job, metric, period, ', node')
+        return state
 
-    # Result is a list of dict, one dict per node. Each dict has it own list of
-    # values. We have to compute the sum the values for all nodes at every
-    # timestamps.
-    #
-    # Ex:
-    # [
-    #   {u'values': [[1441614640000, 0],
-    #                [1441614650000, 3188736.0],
-    #                  ...
-    #                [1441614700000, 3188736.0]],
-    #    u'name': u'memory-pss',
-    #    u'columns': [u'time', u'mean'],
-    #    u'tags': {u'node': u'cn1'}},
-    #   {u'values': [[1441614640000, 0],
-    #                [1441614650000, 3115008.0],
-    #                  ...
-    #                [1441614700000, 3115008.0]],
-    #   u'name': u'memory-pss',
-    #   u'columns': [u'time', u'mean'],
-    #   u'tags': {u'node': u'cn2'}},
-    #  {u'values': [[1441614640000, 0],
-    #               [1441614650000, 3214336.0],
-    #                  ...
-    #               [1441614700000, 3214336.0]],
-    #   u'name': u'memory-pss',
-    #   u'columns': [u'time', u'mean'],
-    #   u'tags': {u'node': u'cn3'}}
-    # ]
+class JobData(object):
 
-    value_idx = 0
-    for value in results[0]['values']:
-        # value in an array with 2 items:
-        #   - a timestamp
-        #   - the metric value at this timestamp
+    def __init__(self, cluster, job, period):
 
-        total = 0
-        for node_serie in results:
-            total += node_serie['values'][value_idx][1]
-        metrics[value[0]].append(total)
-        value_idx += 1
+        self.cluster = cluster
+        self.job = job
+        self.period = period
+        self.nodeset = None
+        self.metrics = None
 
-    node_list = [ node_serie['tags']['node'] for node_serie in results ]
-    ns.updaten(node_list)
+    def request(self, db):
 
-def get_job_metrics(metrics, cluster, job, metric, period):
-    """Get the job metric for the job on the cluster for the period in
-       parameter. Append the result into the metrics dict in parameter.
-    """
+        (self.metrics, self.nodeset) = db.get_metrics_results(
+                                           self.cluster,
+                                           self.job,
+                                           ['cpus',
+                                            'cpu-user',
+                                            'cpu-system',
+                                            'memory-pss'],
+                                           self.period)
+        self.stack_cpu_idle()
 
-    results = get_metric_results(cluster, job, metric, period)
-    for value in results[0]['values']:
-        # value in an array with 2 items:
-        #   - a timestamp
-        #   - the metric value at this timestamp
-        metrics[value[0]] = [ value[1] ]
+    def stack_cpu_idle(self):
+        """Compute the sum of cpu usages in metrics dict in parameters to stack
+           the idle cpu time and append in into the dict.
+        """
 
-def read_conf():
-    """Read/parse configuration file and set global conf variable."""
+        for timestamp, values in self.metrics.iteritems():
+            values.insert(3, values[0]*100 - values[1] - values[2])
 
-    global conf
-    conf = ConfigParser.RawConfigParser()
-    conf.read(CONF)
+    def jsonify(self):
+
+        datahash = {}
+        datahash['data'] = self.metrics
+        datahash['job'] = {}
+        datahash['job']['nodeset'] = str(self.nodeset)
+        return jsonify(datahash)
 
 @app.route('/metrics/<cluster>/<int:job>', defaults={'period': '1h'})
 @app.route('/metrics/<cluster>/<int:job>/<period>')
 def metrics(cluster, job, period):
 
-     read_conf()
-     job_state = get_job_state(cluster, job)
+     conf = Conf()
+     slurm_api = SlurmAPI(conf, cluster)
+     job_state = slurm_api.job_state(job)
 
      if period not in periods.keys():
          abort(500)
@@ -183,19 +258,12 @@ def metrics(cluster, job, period):
          abort(404)
      else:
          try:
-             ns = NodeSet()
-             metrics = {}
-             metrics['data'] = {}
-             get_job_metrics(metrics['data'], cluster, job, 'cpus', period)
-             get_job_node_metrics(metrics['data'], ns, cluster, job, 'cpu-user', period)
-             get_job_node_metrics(metrics['data'], ns, cluster, job, 'cpu-system', period)
-             stack_cpu_idle(metrics['data'])
-             get_job_node_metrics(metrics['data'], ns, cluster, job, 'memory-pss', period)
-             metrics['job'] = {}
-             metrics['job']['nodeset'] = str(ns)
-             return jsonify(metrics)
+             db = MetricsDB(conf)
+             job_data = JobData(cluster, job, period)
+             job_data.request(db)
+             return job_data.jsonify()
          except Exception, e:
-             print(e)
+             print("Error: " + str(e))
              abort(500)
 
 if __name__ == '__main__':
